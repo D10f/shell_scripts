5133a0cdc2730e85b8c2d8b4be3d94330a203c26
herokunt <galactodensimeter_bismuthyl@simplelogin.fr>
2022-02-21 13:39:44 +0100

    Complete creating multiple images, either by name or with -n flag.

--- scw_setup.sh
+++ scw_setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Assumes SCW CLI has been configured previously through docker
-# 
 
 ACC_ZONE="nl-ams-1"
 ACC_PNET="ebde321f-bfab-473f-bbca-8904b995c76a"
@@ -11,8 +10,12 @@ DCK_IMAGE="scaleway/cli:v2.4.0"
 CMD_BASE="docker run -it --rm -v ${HOME}/.config/scw:/root/.config/scw ${DCK_IMAGE} instance"
 
 # Checks if input to create new instances is greater than this value.
-# This prevents long-running scripts and unnecessary charges from your provider
-MAX_INSTANCES=3
+NEW_INSTANCES=1
+MAX_INSTANCES=5
+
+
+######## INIT ########
+
 
 if [[ "${UID}" -ne 1000 ]]
 then
@@ -20,19 +23,81 @@ then
   exit 1
 fi
 
-function usage() {
-  echo "Usage: ${0} [list | create | delete]" >&2
+function usage_general() {
+  echo "Usage: ${0} [list | create | delete] [options]..." >&2
   echo "Manage your account with this wrapper for the official SCW CLI tool."
   exit 1
 }
 
+function usage_create() {
+  echo "Usage: ${0} create [-v] [-n NEW_INSTANCES] [INSTANCE_NAMES]..." >&2
+  echo "Launches any number of instances. Each positional argument will be used"
+  echo "as the name for the instance."
+  echo "  -v                 Increase verbosity."
+  echo "  -n  NEW_INSTANCES  Specify the number of instances to create."
+  echo "  -z  ACC_ZONE       Server location where the instance will be located."
+  echo "  -t  INS_TYPE       Type of server running, default DEV-1."
+  echo "  -i  INS_IMAGE      Image to run in the server, defaults to ubuntu 20.04."
+  exit 1
+}
+
+function usage_delete() {
+  echo "Usage: ${0} delete [-v] [INSTANCES_IDS]..." >&2
+  echo "  -v                    Increase verbosity."
+  exit 1
+}
+
+function usage_list() {
+  echo "Usage: ${0} list [-v] [INSTANCES_IDS]..." >&2
+  echo "  -v                    Increase verbosity."
+  exit 1
+}
+
+
+######## CREATE INSTANCES ########
+
+
+function get_create_arguments() {
+  while getopts vn:z:t:i: OPTION
+  do
+    case ${OPTION} in
+      n)
+        NEW_INSTANCES="${OPTARG}"
+        ;;
+      z)
+        ACC_ZONE="${OPTARG}"
+        ;;
+      t)
+        INS_TYPE="${OPTARG}"
+        ;;
+      i)
+        INS_IMAGE="${OPTARG}"
+        ;;
+      v)
+        echo "Verbosity enabled"
+        ;;
+      ?)
+        usage_create
+        ;;
+    esac
+  done
+}
+
+
 function check_max_instances() {
-  if [[ "${1}" -gt "${MAX_INSTANCES}" ]]
+  
+  NAMED_INSTANCES="$(( $# - 1 ))"
+
+  if [[ "${NAMED_INSTANCES}" -gt "${NEW_INSTANCES}" ]]
+  then
+    NEW_INSTANCES=$NAMED_INSTANCES
+  fi
+
+  if [[ "${NEW_INSTANCES}" -gt "${MAX_INSTANCES}" ]]
   then
     echo "CAUTION!"
-    read -p "Are you sure you want to create ${$1} instances? Type CONFIRM to proceed: " ANSWER
-    
-    if [[ "${ANSWER}" != 'CONFIRM' ]]
+    read -p "Are you sure you want to create ${NEW_INSTANCES} instances? (y/n): " ANSWER    
+    if [[ "${ANSWER}" != 'y' ]]
     then
       echo "Not confirmed: exiting program..."
       exit 1
@@ -42,42 +107,113 @@ function check_max_instances() {
   fi
 }
 
-function list_instances() {
-  local CMD="${CMD_BASE} server list zone=${ACC_ZONE}"
-  $CMD
-}
 
 function create_instances() {
-  read -p "How many instances would you like to create? " NEW_INSTANCES
 
-  check_max_instances ${NEW_INSTANCES}
+  get_create_arguments ${@}
+  shift "$(( OPTIND - 1 ))"
+  check_max_instances ${NEW_INSTANCES} ${@}
 
   START=1
   while [[ "${START}" -le "${NEW_INSTANCES}" ]]
   do
-    echo "Creating instance #${START}"
-    # local CMD="${CMD_BASE} server create type=${INS_TYPE} image=${INS_IMAGE} zone=${ACC_ZONE}"
-    # $CMD
+    echo "Creating instance #${START} (${1})"
+    local CMD="${CMD_BASE} server create type=${INS_TYPE} image=${INS_IMAGE} zone=${ACC_ZONE}"
+
+    if [[ ${1} ]]
+    then
+      CMD="${CMD} name=${1}"
+    fi
+
+    $CMD
     (( START++ ))
+    shift
+  done
+}
+
+
+######## DELETE INSTANCES ########
+
+
+function get_delete_arguments() {
+  while getopts vi: OPTION
+  do
+    case ${OPTION} in
+      i)
+        INSTANCES_IDS="${OPTARG}"
+        ;;
+      v)
+        echo "Verbosity enabled"
+        ;;
+      ?)
+        usage_delete
+        ;;
+    esac
   done
 }
 
 
 function delete_instances() {
+
+  get_delete_arguments ${@}
+
+  # START=1
+  # while [[ "${START}" -le "${INSTANCES_IDS}" ]]
+  # do
+  #   echo "Creating instance #${START}"
+  #   # local CMD="${CMD_BASE} server create type=${INS_TYPE} image=${INS_IMAGE} zone=${ACC_ZONE}"
+  #   # $CMD
+  #   (( START++ ))
+  # done
+
   echo "you called the delete function!"
+  echo "${INSTANCES_IDS}"
 }
+
+
+######## LIST INSTANCES ########
+
+function get_list_arguments() {
+  while getopts vi: OPTION
+  do
+    case ${OPTION} in
+      i)
+        echo "TEST PASSED"
+        ;;
+      v)
+        echo "Verbosity enabled"
+        ;;
+      ?)
+        usage_list
+        ;;
+    esac
+  done
+}
+
+
+function list_instances() {
+
+  get_list_arguments ${@}
+
+  local CMD="${CMD_BASE} server list zone=${ACC_ZONE}"
+  $CMD
+}
+
 
 case "${1}" in
   list)
-    list_instances
+    shift
+    list_instances ${@}
     ;;
   create)
-    create_instances
+    shift
+    create_instances ${@}
     ;;
   delete|remove)
-    delete_instances
+    shift
+    delete_instances ${@}
     ;;
   *)
-    usage
+    usage_general
     ;;
 esac
